b6470f2db67314ad8f4389ae4beabcdc324ee352
KayesAshfaq <asfak7074@gmail.com>
2025-02-17 01:52:33 +0600

    Add Firebase Admin SDK and push notification endpoint

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 
 const app = express();
+const admin = require('firebase-admin');
+const serviceAccount = require('/home/kayes/Downloads/cartupstage-firebase-adminsdk-ke0ra-932332eef3.json'); // Update the path to your service account key file
+
+// Middleware to parse the request body as JSON data
+app.use(express.json());
+
+// Initialize Firebase Admin SDK
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
 const PORT = 3000;
 
 app.listen(PORT, (error) => {
@@ -13,4 +24,25 @@ app.listen(PORT, (error) => {
 app.get('/', (req, res) => {
     res.status(200);
     res.send('Welcome to the root URL of the server');
+});
+
+// Endpoint to send push notification
+app.post('/send-notification', (req, res) => {
+    const { token, message } = req.body;
+
+    const payload = {
+        notification: {
+            title: 'Notification Title',
+            body: message
+        },
+        token: token,
+    };
+
+    admin.messaging().send(payload)
+        .then(response => {
+            res.status(200).send('Notification sent successfully');
+        })
+        .catch(error => {
+            res.status(500).send('Error sending notification: ' + error);
+        });
 });
